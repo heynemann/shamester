5b8fa98fca28c83e9be4a703179898463b7c652f
Evandro Flores <eof@eof.com.br>
2013-09-10 17:41:57 -0300

    add logging

--- shamester_worker/config.py
+++ shamester_worker/config.py
@@ -2,8 +2,13 @@ import os
 from derpconf.config import Config, verify_config
 
 
-Config.define('WORKER_SLEEP_TIME', 60, "Main loop sleep time", 'Worker')
+Config.define("WORKER_SLEEP_TIME", 60, "Main loop sleep time", "Worker")
 
+Config.define("LOG_LEVEL", "ERROR", "Default log level", "Logging")
+Config.define("LOG_FORMAT", "%(asctime)s %(name)s:%(levelname)s %(message)s", 
+	"Log Format to be used when writing log messages", "Logging")
+Config.define("LOG_DATE_FORMAT", "%Y-%m-%d %H:%M:%S",
+    'Date Format to be used when writing log messages.', 'Logging')
 
 def verify_and_load(config_file):
 	verify_config(config_file)
--- shamester_worker/server.py
+++ shamester_worker/server.py
@@ -3,32 +3,57 @@
 
 
 import time
+import logging
+
 from optparse import OptionParser
 from config import Config, verify_and_load
 
+LOGS = {
+    0: 'error',
+    1: 'warning',
+    2: 'info',
+    3: 'debug'
+}
 
 class ShamesterWorkerServer(object):
     config = None
 
+
     def __init__(self, opt):
-       self.config = verify_and_load(opt.conf)
+        self.config = verify_and_load(opt.conf)
+
+        if opt.verbose:
+            log_level = LOGS[opt.verbose].upper() 
+        else: 
+            log_level = self.config.get("LOG_LEVEL")
+
+        logging.basicConfig(
+            level=log_level,
+            format=self.config.get("LOG_FORMAT"),
+            datefmt=self.config.get("LOG_DATE_FORMAT")
+        )
 
 
     def run(self):
         try:
             while True:
-                self.check_new_websites()
+                
+                websites = self.check_new_websites()
+                for website in websites:
+                    logging.debug("Processing [%s]" % website)
+
                 sleep_for = self.config.get('WORKER_SLEEP_TIME')
-                print "Nothing more to do. Will sleep a little bit... (%ss)" % sleep_for
+                logging.debug("Nothing more to do. Will sleep a little bit... (%ss)" % sleep_for)
                 time.sleep(sleep_for)
 
 
         except KeyboardInterrupt:
-            print "\n\nGently stopped to work."
+            logging.info("Gently stopped to work.\nBye.")
 
 
     def check_new_websites(self):
-        print "Checking for new websites..."
+        logging.debug("Checking for new websites...")
+        return ["www.globo.com", "g1.globo.com"]
 
 
 
@@ -37,6 +62,7 @@ def main():
     
     parser.add_option('-c', '--conf', dest='conf', default='../shamester_config/local.conf',
                       help='Configuration file to use for the server.')
+    parser.add_option('--verbose', '-v', action='count', help='Log level: v=warning, vv=info, vvv=debug.')
     
     (opt, args) = parser.parse_args()
 
